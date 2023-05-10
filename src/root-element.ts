8008ac5daf4df49ef38dfa3f37c47c6eca1904db
Jasper O'Leary <jhaazpr@gmail.com>
2023-05-09 17:41:38 -0700

    toolpaths ported to root

--- src/root-element.ts
+++ src/root-element.ts
@@ -1,11 +1,13 @@
 import { LitElement, css, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 import { ToolpathMenu } from './toolpath-menu.ts';
 import { TSSMenu } from './tss-menu.ts';
 import { VisualizationPane } from './visualization-pane.ts';
 import { DebuggingPane } from './debugging-pane.ts';
 import { TSS } from './tss.ts';
 import { Toolpath, IR } from './type-utils.ts';
+import { ExampleToolpaths, ToolpathName } from './example-toolpaths.ts';
+
 import * as THREE from 'three';
 
 import './toolpath-menu.ts';
@@ -17,22 +19,45 @@ import './debugging-pane.ts';
 export class RootElement extends LitElement {
     
     currentTSS: TSS = (_: IR[]) => new THREE.Group();
-    currentToolpath: Toolpath = { isa: 'gcode', instructions: [] };
 
-    tssListener(e: CustomEvent) {
-        this.currentTSS = e.detail.tss;
+    toolpathNames = Object.keys(ExampleToolpaths) as ToolpathName[];
+
+    @property()
+    currentToolpathName: ToolpathName = this.toolpathNames[0];
+
+    @property()
+    currentToolpath: Toolpath = ExampleToolpaths[this.currentToolpathName];
+
+    onToolpathClick(newName: ToolpathName) {
+        this.currentToolpathName = newName;
+        this.currentToolpath = ExampleToolpaths[newName];
     }
 
-    toolpathListener(e: CustomEvent) {
-        this.currentToolpath = e.detail.toolpath;
+    maybeHighlight(name: ToolpathName) {
+        return name === this.currentToolpathName ? 'highlight' : '';
     }
 
     render() {
         return html`
             <div class="container">
                 <div class="menu-col">
-                    <toolpath-menu></toolpath-menu>
-                    <tss-menu @tss-changed=${this.tssListener}></tss-menu>
+                    <div class="menu">
+                        <div class="menu-head">Toolpath Menu</div>
+                        <ul class="toolpath-list">
+                            ${this.toolpathNames.map(name => {
+                                return html`
+                                    <li @click=${() => this.onToolpathClick(name)}
+                                        class=${this.maybeHighlight(name)}>
+                                        ${name}</li>
+                                `;
+                            })}
+                        </ul>
+                        <ul class="toolpath-preview">
+                            ${this.currentToolpath.instructions.map((i: string) => {
+                                return html`<li><code>${i}</code></li>`;
+                            })}
+                        </ul>
+                    </div>
                 </div>
                 <visualization-pane></visualization-pane>
                 <!-- <debugging-pane></debugging-pane> -->
@@ -51,6 +76,44 @@ export class RootElement extends LitElement {
         }
         visualization-pane {
             width: 750px;
+        }
+        .menu {
+            border: 1px solid black;
+            margin: 5px;
+            width: 275px;
+        }
+        .toolpath-list {
+            height: 250px;
+            border: 1px solid black;
+            overflow-y: scroll;
+            padding-left: 5px;
+            margin: 5px;
+        }
+        .toolpath-list li {
+            cursor: pointer;
+            list-style-type: none;
+        }
+        .toolpath-list li:hover {
+            background-color: gray;
+        }
+        .toolpath-preview {
+            border: 1px solid black;
+            margin: 5px;
+            height: 200px;
+            overflow-y: scroll;
+            padding-left: 5px;
+        }
+        .toolpath-preview li {
+            margin-top: -5px;
+            margin-bottom: -5px;
+            list-style-type: none;
+        }
+        .highlight {
+            background-color: black;
+            color: white;
+        }
+        .menu-head {
+            margin: 10px;
         }
     `;
 }
