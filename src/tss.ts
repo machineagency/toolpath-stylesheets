836cbaaaba158cbaa4b506dbf71200b7b91f2091
tmesh17 <thrisha@uw.edu>
2023-05-12 22:22:06 -0700

    change colorVis() function to only run through colors once

--- src/tss.ts
+++ src/tss.ts
@@ -3,13 +3,15 @@ import * as THREE from 'three';
 import { IR } from './type-utils.ts';
 
 export type TSS = (tp: IR[]) => THREE.Group;
-export type TSSName = 'tssA' | 'tssB' | 'tssC' | 'basicVis' | 'colorVis';
+export type TSSName = 'tssA' | 'tssB' | 'tssC' | 'basicVis' | 'distanceFromOriginVis' | 
+                      'distanceTraveledVis';
 
 export type TSSCollection = Record<TSSName, TSS>
 
 export const tssCollection: TSSCollection = {
     basicVis: basicVis,
-    colorVis: colorVis,
+    distanceFromOriginVis: distanceFromOriginVis,
+    distanceTraveledVis: distanceTraveledVis,
     tssA: tssA,
     tssB: tssB,
     tssC: tssC
@@ -54,7 +56,7 @@ function basicVis(irs: IR[]) {
     return group;
 }
 
-function colorVis(irs: IR[]) {
+function distanceFromOriginVis(irs: IR[]) {
     let moveCurves: THREE.LineCurve3[] = [];
     let currentPos = new THREE.Vector3();
     irs.forEach(function (ir) {
@@ -85,7 +87,7 @@ function colorVis(irs: IR[]) {
             let point = new THREE.Vector3(points[i], points[i + 1], points[i + 2]);
             let distance = point.distanceTo(new THREE.Vector3());
 
-            let hue = distance / 100; // changes rainbow coloring
+            let hue = distance / 300; // changes rainbow coloring
             color.setHSL(hue, 1.0, 0.5);
             colors.push(color.r, color.g, color.b);
         }
@@ -100,3 +102,58 @@ function colorVis(irs: IR[]) {
     group.rotateX(Math.PI / 2);
     return group;
 }
+
+function distanceTraveledVis(irs: IR[]) {
+    let moveCurves: THREE.LineCurve3[] = [];
+    let currentPos = new THREE.Vector3();
+    irs.forEach(function (ir) {
+        let newPos = new THREE.Vector3(
+            ir.args.x || currentPos.x,
+            ir.args.y || currentPos.y,
+            ir.args.z || currentPos.z
+        );
+        let moveCurve = new THREE.LineCurve3(currentPos, newPos);
+        moveCurves.push(moveCurve);
+        currentPos = newPos;
+    });
+
+    let lines = moveCurves.map(curve => {
+        let geometry = new THREE.BufferGeometry().setFromPoints(curve.getPoints(50));
+        let material = new THREE.LineBasicMaterial({ vertexColors: true });
+        return new THREE.Line(geometry, material);
+    });
+
+    let group = new THREE.Group();
+
+    let totalLength = 0;
+    moveCurves.forEach(curve => {
+        totalLength += curve.getLength();
+    });
+
+    let distance = 0;
+    lines.forEach((line) => {
+        let points= line.geometry.attributes.position.array;
+        let colors = [];
+        let color = new THREE.Color();
+            
+        for (let i = 0; i < points.length; i += 3) {
+            let point = new THREE.Vector3(points[i], points[i + 1], points[i + 2]);
+            let segmentLength = point.distanceTo(currentPos);
+            distance += segmentLength;
+            currentPos = point;
+            let hue = distance / totalLength;
+            color.setHSL(hue, 1.0, 0.5);
+            colors.push(color.r, color.g, color.b);
+        }
+
+        line.geometry.setAttribute(
+            'color',
+            new THREE.BufferAttribute(new Float32Array(colors), 3)
+        );
+
+        group.add(line);
+    });
+    group.rotateX(Math.PI / 2);
+    return group;
+  }
+  
