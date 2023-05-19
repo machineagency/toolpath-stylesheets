cd45d12ee4140b2addddc691930a50f85a08dd34
tmesh17 <thrisha@uw.edu>
2023-05-19 00:09:40 -0700

    remove blank tss functions and commented out code

--- src/tss.ts
+++ src/tss.ts
@@ -3,33 +3,14 @@ import * as THREE from 'three';
 import { IR } from './type-utils.ts';
 
 export type TSS = (tp: IR[]) => THREE.Group;
-export type TSSName = 'tssA' | 'tssB' | 'tssC' | 'basicVis' |
-                      'distanceTraveledVis';
+export type TSSName = 'basicVis' | 'distanceTraveledVis';
 
 export type TSSCollection = Record<TSSName, TSS>
 
 export const tssCollection: TSSCollection = {
     basicVis: basicVis,
-    distanceTraveledVis: distanceTraveledVis,
-    tssA: tssA,
-    tssB: tssB,
-    tssC: tssC
+    distanceTraveledVis: distanceTraveledVis
 };
-
-function tssA(tp: IR[]) {
-    tp;
-    return new THREE.Group();
-}
-
-function tssB(tp: IR[]) {
-    tp;
-    return new THREE.Group();
-}
-
-function tssC(tp: IR[]) {
-    tp;
-    return new THREE.Group();
-}
 
 function basicVis(irs: IR[]) {
     let moveCurves: THREE.LineCurve3[] = [];
@@ -74,58 +55,6 @@ function basicVis(irs: IR[]) {
     group.rotateX(Math.PI / 2);
     return group;
 }
-
-/*
-function distanceFromOriginVis(irs: IR[]) {
-    let moveCurves: THREE.LineCurve3[] = [];
-    let currentPos = new THREE.Vector3();
-    irs.forEach(function (ir) {
-      let newPos = new THREE.Vector3(
-        ir.args.x || currentPos.x,
-        ir.args.y || currentPos.y,
-        ir.args.z || currentPos.z
-      );
-      let moveCurve = new THREE.LineCurve3(currentPos, newPos);
-      moveCurves.push(moveCurve);
-      currentPos = newPos;
-    });
-
-    let lines = moveCurves.map(curve => {
-        let geometry = new THREE.BufferGeometry().setFromPoints(curve.getPoints(50));
-        let material = new THREE.LineBasicMaterial({ vertexColors: true });
-        return new THREE.Line(geometry, material);
-    });
-
-    let group = new THREE.Group();
-
-    lines.forEach((line) => {
-        let points= line.geometry.attributes.position.array;
-        let colors = [];
-        let color = new THREE.Color();
-        
-        for (let i = 0; i < points.length; i += 3) {
-            let point = new THREE.Vector3(points[i], points[i + 1], points[i + 2]);
-            let distance = point.distanceTo(new THREE.Vector3());
-
-            let hue = distance / 300; // changes rainbow coloring
-            color.setHSL(hue, 1.0, 0.5);
-            colors.push(color.r, color.g, color.b);
-        }
-
-        line.geometry.setAttribute(
-            'color',
-            new THREE.BufferAttribute(new Float32Array(colors), 3)
-        );
-
-        group.add(line);
-    });
-    group.rotateX(Math.PI / 2);
-    if (irs[0].state.units === 'in') {
-        group.scale.set(25.4, 25.4, 25.4); // in to mm converstion
-    }
-    return group;
-}
-*/
 
 function distanceTraveledVis(irs: IR[]) {
     let moveCurves: THREE.LineCurve3[] = [];
