df0c39688f8e6b79b1c02196bb7f12a687329a6c
Jasper O'Leary <jhaazpr@gmail.com>
2024-01-26 14:31:13 -0800

    update vite etc to use react

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
+import { defineConfig } from 'vite';
+import React from '@vitejs/plugin-react';
+
 // vite.config.js
 import checker from 'vite-plugin-checker'
-export default {
+export default defineConfig({
   plugins: [
+    React(),
     checker({
       // e.g. use TypeScript check
       typescript: true,
     }),
   ],
-}
+});
