3a47cbbfc35d6eade8d94afb56213fd4fc2901fb
Jasper O'Leary <jhaazpr@gmail.com>
2024-01-26 14:31:30 -0800

    boilerplate trajectory page

---
+++ src/trajectory.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default function TrajectoryWindow() {
+    return <div>Testing testing 1, 2, 3</div>
+};
+
+ReactDOM.render(
+    <TrajectoryWindow></TrajectoryWindow>,
+    document.getElementById("app")
+);
