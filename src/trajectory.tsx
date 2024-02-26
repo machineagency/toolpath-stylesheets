23f5086119b49e08bdd2cc64c4d468a097831dbb
Jasper O'Leary <jhaazpr@gmail.com>
2024-02-26 11:29:33 -0800

    break long lines, delete commented code

--- src/trajectory.tsx
+++ src/trajectory.tsx
@@ -46,14 +46,13 @@ const TOOLPATH_TABLE: Record<string, Toolpath> = {
     testToolpath: toolpath("sbp", ["M2, 0, 0", "M2, 10, 10"]),
     rectangle: toolpath("sbp", ["M2, 0, 0", "M2, 10, 0", "M2, 10, 5", "M2, 0, 5", "M2, 0, 0"]),
     triangle: toolpath("sbp", ["M2, 0, 0", "M2, 10, 0", "M2, 5, 5", "M2, 0, 0"]),
-    star: toolpath("sbp", ["M2, 50, 0", "M2, 65, 35", "M2, 100, 35", "M2, 75, 60", "M2, 90, 95", "M2, 50, 75",
-                   "M2, 10, 95", "M2, 25, 60", "M2, 0, 35", "M2, 35, 35", "M2, 50, 0"]),
+    star: toolpath("sbp", ["M2, 50, 0", "M2, 65, 35", "M2, 100, 35", "M2, 75, 60",
+                    "M2, 90, 95", "M2, 50, 75", "M2, 10, 95", "M2, 25, 60", "M2, 0, 35",
+                    "M2, 35, 35", "M2, 50, 0"]),
     wave: exampleToolpaths.gCodeWave,
     box: exampleToolpaths.ebbBox,
     signature: exampleToolpaths.ebbSignature,
     gears: exampleToolpaths.gears
-
-
 };
 
 function DashboardSettings({ onSelect }: DashboardSettingsProps) {
@@ -146,8 +145,10 @@ function ProfilePlot({ lineSegments, min, max }: ProfilePlotProps) {
         });
 
         cumulativeTimes = cumulativeTimes.slice(min, max);
-        const vPairs = lineSegments.map((ls: LineSegment) => [ls.profile.v0, ls.profile.v, null]).flat().slice(min, max);
-        const aPairs = lineSegments.map((ls: LineSegment) => [ls.profile.a, ls.profile.a, null]).flat().slice(min, max);
+        const vPairs = lineSegments.map((ls: LineSegment) => [ls.profile.v0, ls.profile.v, null])
+            .flat().slice(min, max);
+        const aPairs = lineSegments.map((ls: LineSegment) => [ls.profile.a, ls.profile.a, null])
+            .flat().slice(min, max);
 
         const plot = Plot.plot({
             x: {
@@ -268,7 +269,7 @@ function lineSegment(parent: number, start: Coords, end: Coords, unit: Coords,
     }
 }
 
-function kinematicLimits(maxVelocity: Coords, maxAcceleration: Coords, 
+function kinematicLimits(maxVelocity: Coords, maxAcceleration: Coords,
          junctionSpeed: number, junctionDeviation: number): KinematicLimits {
     return {
         vMax: maxVelocity,
@@ -289,7 +290,8 @@ function firstOrder(initialVelocity: number, finalVelocity: number, acceleration
     }
 }
 
-function planTriplets(locations: Segment[], prePlanned: LineSegment[], halfPlanned: LineSegment[], fullyPlanned: LineSegment[]): TrajectoryPasses {
+function planTriplets(locations: Segment[], prePlanned: LineSegment[],
+    halfPlanned: LineSegment[], fullyPlanned: LineSegment[]): TrajectoryPasses {
     return {
         locations: locations,
         prePlanned: prePlanned,
@@ -298,7 +300,8 @@ function planTriplets(locations: Segment[], prePlanned: LineSegment[], halfPlann
     }
 }
 
-function fromGeo(parent: number, v0: number, v1: number, start: Coords, end: Coords, k1: KinematicLimits): LineSegment {
+function fromGeo(parent: number, v0: number, v1: number, start: Coords, end: Coords,
+    k1: KinematicLimits): LineSegment {
     let startVel = Math.abs(v0);
     let endVel = Math.abs(v1);
     let delta = coords(end.x - start.x, end.y - start.y)
@@ -317,7 +320,8 @@ function limitVector(v: Coords, l: Coords): number {
 }
 
 
-function normalize(v0: number | null, v: number | null, a: number | null, t: number | null, x: number | null): FirstOrder | null {
+function normalize(v0: number | null, v: number | null, a: number | null,
+                    t: number | null, x: number | null): FirstOrder | null {
     let arr = [v0, v, a, t, x];
     if ((arr.length - arr.filter(element => element !== null && !isNaN(element)).length) != 2) {
         return null;
@@ -362,46 +366,6 @@ function normalize(v0: number | null, v: number | null, a: number | null, t: num
     let time = (v - v0!) / a!;
     return firstOrder(v0!, v, a!, time, time * (v0! + v) / 2);
 }
-
-/*
-function makeTestSegment(n: number): TrajectoryPasses {
-    let segments: Segment[] = [segment(0, 1.0, 1.0, coords(1.0, 0.0))];
-    let arr: number[] = linspace(0, Math.PI / 2, n);
-
-    arr.forEach(function (val: number, index: number) {
-        let seg = segment(index + 1, 2.0, 2.0, coords(Math.cos(val), Math.sin(val)));
-        segments.push(seg);
-    });
-
-    let end: Segment = segment(n + 1, 3.0, 3.0, coords(0, -5));
-    segments.push(end);
-
-    let limits = kinematicLimits(coords(1.0, 1.0), coords(1.0, 1.0), 1e-3, 1e-2);
-    let startLocation = coords(0, 0);
-    let plannerSegments: LineSegment[] = [];
-
-    segments.forEach(function (s: Segment) {
-        let endLocation = s.coords;
-        let segmentNorm = number(norm([startLocation.x - endLocation.x, startLocation.y - endLocation.y]));
-
-        if (segmentNorm >= 1e-18) {
-            let segment = fromGeo(s.moveId, s.startVelocity, s.endVelocity, startLocation, endLocation, limits);
-            plannerSegments.push(segment);
-            startLocation = endLocation;
-        }
-    });
-
-    let halfPlanned = forwardPass(plannerSegments, 0, limits);
-
-    let plannedSegments: LineSegment[] = [];
-
-    planSegments(plannerSegments, limits).forEach(function (s: LineSegment) {
-        plannedSegments.push(s);
-    });
-
-    return planTriplets(segments, plannerSegments, halfPlanned, plannedSegments);
-}
-*/
 
 function main(tp: Toolpath): TrajectoryPasses {
     let irs;
@@ -581,18 +545,6 @@ function limitValueByAxis(limits: Coords, vector: Coords): number {
     return limitValue;
 }
 
-/*
-function linspace(start: number, stop: number, cardinality: number): number[] {
-    let arr: number[] = [];
-    let step = (stop - start) / (cardinality - 1);
-    for (let i = 0; i < cardinality; i++) {
-        arr.push(start + (step * i));
-    }
-
-    return arr;
-}
-*/
-
 function TrajectoryWindow({ toolpath, min, max }: TrajectoryWindowProps) {
     // TODO: do all the planning using the toolpath parameter passed in the props
     // let { locations, prePlanned, halfPlanned, fullyPlanned} = makeTestSegment(20);
@@ -657,7 +609,6 @@ function App() {
         // Update maxValue whenever currentToolpath changes
         setMaxValue(max);
     }, [max, currentToolpath]);
-    
 
     const handleRangeChange = (newRange: number[] | number) => {
         let range = newRange as number[];
